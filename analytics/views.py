90c7b1c53192fd815dcca7c099e0f291005d529b
Shaun Sephton <shaunsephton@gmail.com>
2011-07-27 15:00:49 +0200

    shell for default view

--- analytics/views.py
+++ analytics/views.py
@@ -1,3 +1,8 @@
-# No views yet.
+from django.views.generic import TemplateView
 
+class AnalyticsView(TemplateView):
+    def get_template_names(self):
+        return 'analytics/dashboard.html'
 
+class DefaultAnalyticsView(AnalyticsView):
+    pass
