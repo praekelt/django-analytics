3da135656ced03df3c11716692b6cddc212de4ba
Shaun Sephton <shaunsephton@gmail.com>
2011-07-27 15:01:18 +0200

    default view url. pep8

--- analytics/urls.py
+++ analytics/urls.py
@@ -7,10 +7,10 @@
 from django.conf.urls.defaults import *
 from analytics.geckoboard_views import *
 from analytics.csv_views import *
-
+from analytics import views
 
 urlpatterns = patterns('',
-    # geckoboard-related views
+    # Geckoboard-related views.
     (r'^geckoboard/numbers', geckoboard_number_widget),
     (r'^geckoboard/rag', geckoboard_rag_widget),
     (r'^geckoboard/pie', geckoboard_pie_chart),
@@ -18,7 +18,10 @@ urlpatterns = patterns('',
     (r'^geckoboard/geckometer', geckoboard_geckometer),
     (r'^geckoboard/funnel', geckoboard_funnel),
 
-    # CSV-realted views
+    # CSV related views.
     (r'^csv/(?P<uid>[a-zA-Z0-9\_]+)', csv_dump),
+
+    # Default analytics dashboard.
+    url(r'^default', views.DefaultAnalyticsView.as_view(), name='default_analytics_views'),
 )
 
