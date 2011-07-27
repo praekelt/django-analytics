2ea9320f3e3a1b8a68ea8faf597f243e10645576
Shaun Sephton <shaunsephton@gmail.com>
2011-07-27 16:43:35 +0200

    collect widgets

--- analytics/views.py
+++ analytics/views.py
@@ -1,8 +1,21 @@
 from django.views.generic import TemplateView
+from analytics.sites import metrics
 
 class AnalyticsView(TemplateView):
     def get_template_names(self):
         return 'analytics/dashboard.html'
+    
+    def get_context_data(self, **kwargs):
+        """
+        Get the context for this view.
+        """
+        widgets = []
+        for metric in metrics._registry:
+            widgets.append(metric.widget)
+
+        context = {'widgets': widgets}
+        context.update(kwargs)
+        return context
 
 class DefaultAnalyticsView(AnalyticsView):
     pass
