f2571fc16c0b1801ce2d9cbad731cc703ca3b961
Shaun Sephton <shaunsephton@gmail.com>
2011-07-27 16:44:16 +0200

    less severe failure

--- analytics/urls.py
+++ analytics/urls.py
@@ -1,27 +1,18 @@
-#
-# URLs for django-analytics.
-#
-# thane@praekelt.com
-#
-
 from django.conf.urls.defaults import *
-from analytics.geckoboard_views import *
-from analytics.csv_views import *
 from analytics import views
 
 urlpatterns = patterns('',
     # Geckoboard-related views.
-    (r'^geckoboard/numbers', geckoboard_number_widget),
-    (r'^geckoboard/rag', geckoboard_rag_widget),
-    (r'^geckoboard/pie', geckoboard_pie_chart),
-    (r'^geckoboard/line', geckoboard_line_chart),
-    (r'^geckoboard/geckometer', geckoboard_geckometer),
-    (r'^geckoboard/funnel', geckoboard_funnel),
+    (r'^geckoboard/numbers', 'analytics.geckoboard_views.geckoboard_number_widget'),
+    (r'^geckoboard/rag', 'analytics.geckoboard_views.geckoboard_rag_widget'),
+    (r'^geckoboard/pie', 'analytics.geckoboard_views.geckoboard_pie_chart'),
+    (r'^geckoboard/line', 'analytics.geckoboard_views.geckoboard_line_chart'),
+    (r'^geckoboard/geckometer', 'analytics.geckoboard_views.geckoboard_geckometer'),
+    (r'^geckoboard/funnel', 'analytics.geckoboard_views.geckoboard_funnel'),
 
     # CSV related views.
-    (r'^csv/(?P<uid>[a-zA-Z0-9\_]+)', csv_dump),
+    (r'^csv/(?P<uid>[a-zA-Z0-9\_]+)', 'analytics.csv_views.csv_dump'),
 
     # Default analytics dashboard.
     url(r'^default', views.DefaultAnalyticsView.as_view(), name='default_analytics_views'),
 )
-
