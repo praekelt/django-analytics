32a5f1f084af48e0d7ffd95d70b3a9a6b2fb51c6
Shaun Sephton <shaunsephton@gmail.com>
2011-07-28 10:38:07 +0200

    test hookup

---
+++ runtests.py
@@ -0,0 +1,19 @@
+import sys
+from django.conf import settings
+ 
+if not settings.configured:
+    settings.configure(
+        DATABASE_ENGINE='sqlite3',
+        INSTALLED_APPS=[
+            'analytics',
+        ],
+    )
+ 
+from django.test.simple import run_tests
+ 
+def runtests(*test_args):
+    failures = run_tests(test_args, verbosity=1, interactive=True)
+    sys.exit(failures)
+ 
+if __name__ == '__main__':
+    runtests(*sys.argv[1:])
