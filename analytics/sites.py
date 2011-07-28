1334c9673f795832886de45670752d03e89e391d
Shaun Sephton <shaunsephton@gmail.com>
2011-07-28 09:33:19 +0200

    renamed to gadgets, cleanup

--- analytics/sites.py
+++ analytics/sites.py
@@ -1,32 +1,22 @@
-from django.conf import settings
-
 class AlreadyRegistered(Exception):
     pass
 
-class Metrics(object):
+class Gadgets(object):
     """
-    A Metrics object providing managing various metrics.
-    Models are registered with the Metrics using the register() method.
+    A Gadgets object providing managing of various gadgets for display on analytics dashbaord.
+    Gadgets are registered with the Gadgets using the register() method.
     """
     def __init__(self):
-        self._registry = [] # metric class -> stats model class.
+        self._registry = [] # gadget object.
 
-    def register(self, metric_class):
+    def register(self, gadget):
         """
-        Registers the metric class.
-        If a metric class is already registered, this will raise AlreadyRegistered.
+        Registers a gadget object.
+        If a gadget is already registered, this will raise AlreadyRegistered.
         """
-        # Don't validate unless required.
-        if settings.DEBUG:
-            from analytics.validation import validate
-            validate(metric_class)
-
-        # Instantiate the metric class to save in the registry
-        if metric_class in self._registry:
+        if gadget in self._registry:
             raise AlreadyRegistered
         else:
-            self._registry.append(metric_class())
+            self._registry.append(gadget)
     
-# This global metrics represents the default metrics, for the common case.
-# You can instantiate Metrics in your own code to create a custom metrics object.
-metrics = Metrics()
+gadgets = Gadgets()
