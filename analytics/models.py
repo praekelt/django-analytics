74571ccb610927ee68dc3ed2a2f82cf1f737c57b
Thane Thomson <thane@praekelt.com>
2011-07-29 10:33:52 +0200

    changed calculate method to a classmethod

--- analytics/models.py
+++ analytics/models.py
@@ -6,15 +6,10 @@ class Registrations(options.Statistic):
     """
     Monitors the number of new user signups.
     """
-    label = 'Registrations'
 
-    def calculate(self, start_datetime, end_datetime):
+    @classmethod
+    def calculate(cls, start_datetime, end_datetime):
         return User.objects.filter(date_joined__gte=start_datetime,
             date_joined__lt=end_datetime).count()
 
-    # XXX: This should not have to be here.
-    #def get_earliest_timestamp(self):
-    #    try:
-    #        return User.objects.order_by('date_joined')[0].date_joined
-    #    except IndexError:
-    #        return None
+
