29c22d03374ccc0ec451650e2c2886d324f6e5c6
Shaun Sephton <shaunsephton@gmail.com>
2011-08-15 16:00:38 +0200

    refactor

--- analytics/gadgets.py
+++ analytics/gadgets.py
@@ -7,16 +7,17 @@ from analytics.views import dashboard
 from analytics.sites import gadgets
 
 class BaseGadget(object):
-    def __init__(self, title, stats, display_type, frequency, samples, width, height):
+    def __init__(self, title, stats, display_type, frequency, samples, columns, rows):
         self.title = title
         # make sure we have a list of statistics
         self.stats = list(stats) if getattr(stats, '__iter__', False) else [stats]
         self.display_type = display_type
         self.frequency = frequency
         self.samples = samples
-        self.width = width
-        self.height = height
-        self.id = None
+        self.columns = columns
+        self.rows = rows
+        self.id = self.__hash__()
+        gadgets.register(self)
 
     def get_data(self):
         from datetime import datetime
@@ -26,19 +27,23 @@ class BaseGadget(object):
         data = []
         for stat in self.stats:
             latest = stat.get_latest(frequency=self.frequency)
-            #data.append((time.mktime(latest.date_time.timetuple()), getattr(stat.get_latest(), self.display_type)))
-            data.append((time.mktime(datetime.now().timetuple()) * 1000, randint(0, 10000)))
+            #data.append((time.mktime(latest.date_time.timetuple()) * 1000, latest.count))
+            data.append((time.mktime(datetime.now().timetuple()) * 1000, randint(0, 10)))
 
         return data
         return [(time.mktime(datetime.now().timetuple()) * 1000, randint(0, 10000)), (time.mktime(datetime.now().timetuple()) * 1000, randint(0, 10000))]
-    
-    def render(self, template):
-        gadgets.register(self)
+   
+    def render_js_funcs(self):
         context = {
             'object': self,
-            'hash': self.__hash__(),
         }
-        return render_to_string(template, context)
+        return render_to_string(self.js_funcs_template, context)
+    
+    def render_js_init(self):
+        context = {
+            'object': self,
+        }
+        return render_to_string(self.js_init_template, context)
 
     def __str__(self):
         if hasattr(self, '__unicode__'):
@@ -52,8 +57,12 @@ class BarGadget(BaseGadget):
         super(BarGadget, self).render('analytics/gadgets/bar.html')
 
 class LineGadget(BaseGadget):
+    js_funcs_template = 'analytics/gadgets/line/funcs.js'
+    js_init_template = 'analytics/gadgets/line/init.js'
+    """
     def render(self):
         return super(LineGadget, self).render('analytics/gadgets/line.html')
+    """
 
 class NumberGadget(BaseGadget):
     def render(self):
