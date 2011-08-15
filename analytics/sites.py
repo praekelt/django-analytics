6835cd7efc31447bf2eb83c6feab91a9615897e5
Shaun Sephton <shaunsephton@gmail.com>
2011-08-15 15:58:01 +0200

    id registration

--- analytics/sites.py
+++ analytics/sites.py
@@ -1,13 +1,13 @@
 class Gadgets(object):
     """
-    A Gadgets object providing managing of various gadgets for display on analytics dashbaord.
+    A Gadgets object providing managing of various gadgets for display on analytics dashboard.
     Gadgets are registered with the Gadgets using the register() method.
     """
     def __init__(self):
         self._registry = {} # gadget hash -> gadget object.
 
-    def get_gadget(self, hash):
-        return self._registry[hash]
+    def get_gadget(self, id):
+        return self._registry[id]
 
     def get_gadgets(self):
         return self._registry.values()
@@ -17,6 +17,6 @@ class Gadgets(object):
         Registers a gadget object.
         If a gadget is already registered, this will raise AlreadyRegistered.
         """
-        self._registry[gadget.__hash__()] = gadget
+        self._registry[gadget.id] = gadget
 
 gadgets = Gadgets()
